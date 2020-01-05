3ce38cceb9755822b845aca6347bf3862f6ad022
Dang <shoop0830@gmail.com>
2020-01-05 17:26:10 +0800

    Git Init

---
+++ WatchDog/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace WatchDog
+{
+    partial class Frm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lb_BytesReceivedPerSec = new System.Windows.Forms.Label();
+            this.lb_BytesReceivedAvg = new System.Windows.Forms.Label();
+            this.tb_log = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tb_threshold = new System.Windows.Forms.TextBox();
+            this.lb_Status = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lb_BytesReceivedPerSec
+            // 
+            this.lb_BytesReceivedPerSec.AutoSize = true;
+            this.lb_BytesReceivedPerSec.Location = new System.Drawing.Point(20, 20);
+            this.lb_BytesReceivedPerSec.Name = "lb_BytesReceivedPerSec";
+            this.lb_BytesReceivedPerSec.Size = new System.Drawing.Size(117, 12);
+            this.lb_BytesReceivedPerSec.TabIndex = 0;
+            this.lb_BytesReceivedPerSec.Text = "BytesReceivedPerSec：";
+            // 
+            // lb_BytesReceivedAvg
+            // 
+            this.lb_BytesReceivedAvg.AutoSize = true;
+            this.lb_BytesReceivedAvg.Location = new System.Drawing.Point(20, 45);
+            this.lb_BytesReceivedAvg.Name = "lb_BytesReceivedAvg";
+            this.lb_BytesReceivedAvg.Size = new System.Drawing.Size(106, 12);
+            this.lb_BytesReceivedAvg.TabIndex = 0;
+            this.lb_BytesReceivedAvg.Text = "BytesReceivedAvg：";
+            // 
+            // tb_log
+            // 
+            this.tb_log.Location = new System.Drawing.Point(20, 126);
+            this.tb_log.Multiline = true;
+            this.tb_log.Name = "tb_log";
+            this.tb_log.Size = new System.Drawing.Size(345, 336);
+            this.tb_log.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 100);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Threshold：";
+            // 
+            // tb_threshold
+            // 
+            this.tb_threshold.ImeMode = System.Windows.Forms.ImeMode.Off;
+            this.tb_threshold.Location = new System.Drawing.Point(85, 94);
+            this.tb_threshold.MaxLength = 10;
+            this.tb_threshold.Name = "tb_threshold";
+            this.tb_threshold.Size = new System.Drawing.Size(99, 22);
+            this.tb_threshold.TabIndex = 2;
+            this.tb_threshold.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Tb_threshold_KeyPress);
+            // 
+            // lb_Status
+            // 
+            this.lb_Status.AutoSize = true;
+            this.lb_Status.Location = new System.Drawing.Point(21, 75);
+            this.lb_Status.Name = "lb_Status";
+            this.lb_Status.Size = new System.Drawing.Size(44, 12);
+            this.lb_Status.TabIndex = 0;
+            this.lb_Status.Text = "Status：";
+            // 
+            // Frm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(395, 473);
+            this.Controls.Add(this.tb_threshold);
+            this.Controls.Add(this.tb_log);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lb_Status);
+            this.Controls.Add(this.lb_BytesReceivedAvg);
+            this.Controls.Add(this.lb_BytesReceivedPerSec);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Frm";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "qBittorrent Watch Dog";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lb_BytesReceivedPerSec;
+        private System.Windows.Forms.Label lb_BytesReceivedAvg;
+        private System.Windows.Forms.TextBox tb_log;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tb_threshold;
+        private System.Windows.Forms.Label lb_Status;
+    }
+}
+
---
+++ WatchDog/Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Management;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace WatchDog
+{
+    public partial class Frm : Form
+    {
+        System.Windows.Forms.Timer UpdataTimer       = new System.Windows.Forms.Timer();
+        System.Windows.Forms.Timer CheckTimer        = new System.Windows.Forms.Timer();
+        System.Windows.Forms.Timer CheckProcessTimer = new System.Windows.Forms.Timer();
+        List<int> ls_Avg = new List<int>();
+        int Count        = 0;
+        string log       = "";
+        const int course = 600; //取樣600個，1秒取一次等於10分鐘取樣
+
+        public Frm()
+        {
+            InitializeComponent();
+
+            //更新下載流量timer
+            UpdataTimer.Interval = 1000; // 1sec
+            UpdataTimer.Tick    += new EventHandler(this.OnUpdateInfoDate);
+            UpdataTimer.Enabled  = true;
+
+            //確認流量是否低於門檻值
+            CheckTimer.Interval = 600000; //10 min
+            CheckTimer.Tick    += new EventHandler(this.OnCheckDate);
+            CheckTimer.Enabled  = true;
+
+            //確認軟體是否存在，如果被關掉就會重新開啟
+            CheckProcessTimer.Interval = 60000; //1 min
+            CheckProcessTimer.Tick    += new EventHandler(this.OnCheckProcess);
+            CheckProcessTimer.Enabled  = true;
+        }
+
+        public void OnCheckDate(object sender, EventArgs e)
+        {
+            int Threshold;
+
+            if (int.TryParse(tb_threshold.Text, out Threshold))
+            {
+                int dataAvg = (int)ls_Avg.Average();
+
+                if (Count != 0 && // 總計數大於零
+                    dataAvg < Threshold) // 門檻值小於平均流量
+                {
+                    log = string.Format("{0}{1} {2}: Restart | {3}/{4}\r\n",
+                        log,
+                        DateTime.Now.ToShortDateString(),
+                        DateTime.Now.TimeOfDay,
+                        Threshold,
+                        dataAvg);
+
+                    tb_log.Text = log;
+                    ResetData();
+                    Restart();
+                }
+            }
+        }
+
+        private void OnCheckProcess(object sender, EventArgs e)
+        {
+            Process[] qBittorrentPrt;
+            qBittorrentPrt = Process.GetProcessesByName("qbittorrent");
+            if (qBittorrentPrt.Length == 0)
+            {
+                Runqbittorrent();
+            }
+        }
+
+        private void Restart()
+        {
+            Process[] qBittorrentPrt;
+            qBittorrentPrt = Process.GetProcessesByName("qbittorrent");
+            foreach (Process item in qBittorrentPrt)
+            {
+                try
+                {
+                    item.Kill();
+                    Thread.Sleep(1000);
+                    Runqbittorrent();
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("{0} Exception caught.", e);
+                }
+            }
+        }
+
+        private void Runqbittorrent()
+        {
+            Process memProcess = new Process();
+            memProcess.StartInfo.FileName = "C:\\Program Files\\qBittorrent\\qbittorrent.exe";
+            memProcess.StartInfo.UseShellExecute = true;
+            memProcess.Start();
+        }
+
+        private void ResetData()
+        {
+            ls_Avg.Clear();
+            Count = 0;
+        }
+
+        public void OnUpdateInfoDate(object sender, EventArgs e)
+        {
+            int dataPerSec = GetBytesReceivedPerSec();
+
+            if (ls_Avg.Count <= course)
+            {
+
+                ls_Avg.Add(dataPerSec);
+
+            }
+            else
+            {
+                ls_Avg[Count] = dataPerSec;
+                Count++;
+                if (Count >= course) Count = 1;
+            }
+
+            lb_BytesReceivedPerSec.Text = string.Format("BytesReceivedPerSec：{0} Kbps", dataPerSec);
+            lb_BytesReceivedAvg.Text    = string.Format("BytesReceivedAvg：{0:F2} Kbps", ls_Avg.Average());
+            lb_Status.Text              = string.Format("Status：ListCount：{0} | Course：{1}/{2}", ls_Avg.Count, Count, course);
+        }
+
+        static private int GetBytesReceivedPerSec()
+        {
+            int Kbps = 0;
+            UInt64 bps = 0;
+
+            ObjectQuery winQuery = new ObjectQuery("SELECT * FROM Win32_PerfFormattedData_Tcpip_NetworkInterface");
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher(winQuery);
+            foreach (ManagementObject item in searcher.Get())
+            {
+                bps += Convert.ToUInt64(item["BytesReceivedPerSec"]);
+            }
+            Kbps = (int)(bps / 1024 * 0.8); //bps Conver to Kbps
+            
+            return Kbps;
+        }
+
+        private void Tb_threshold_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+
+            if ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
